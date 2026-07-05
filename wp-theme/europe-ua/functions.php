79505b8c078456aad4c26569b34c831b95f79475
shternamber-eng <shternamber@gmail.com>
2026-07-05 10:48:41 -0400

    Fix permalinks to /%postname%/ so pretty REST API URLs stop 404ing

--- wp-theme/europe-ua/functions.php
+++ wp-theme/europe-ua/functions.php
@@ -6,6 +6,18 @@ function europe_ua_setup() {
 }
 add_action('after_setup_theme', 'europe_ua_setup');
 
+// Одноразовий фікс: сайт був на "простих" permalinks (?p=123), через що
+// /wp-json/... REST-шляхи віддавали 404. Перемикаємо на /%postname%/ і
+// скидаємо rewrite-правила. Ідемпотентно — після першого спрацювання
+// нічого не пише і не викликає flush повторно.
+function europe_ua_ensure_pretty_permalinks() {
+    if (get_option('permalink_structure') !== '/%postname%/') {
+        update_option('permalink_structure', '/%postname%/');
+        flush_rewrite_rules();
+    }
+}
+add_action('init', 'europe_ua_ensure_pretty_permalinks');
+
 function europe_ua_enqueue_assets() {
     wp_enqueue_style('europe-ua-fonts', 'https://fonts.googleapis.com/css2?family=Unbounded:wght@500;700&display=swap', [], null);
     wp_enqueue_style('europe-ua-style', get_stylesheet_uri(), [], wp_get_theme()->get('Version'));
